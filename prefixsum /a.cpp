f87bacae078bc2c21747d919a83988544fbc002b
Kimdonghwan <kimdonghwan913@gmail.com>
2024-09-26 16:06:53 +0900

    implement

---
+++ prefixsum /a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[100004], b , c, psum[100004], n, m;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+    for(int i=1; i<=n; i++){
+        cin >> a[i];
+        psum[i] = psum[i - 1] + a[i];
+    }
+    for (int i=0; i<=n; i++){
+        cout << a[i] << '\n';
+    }
+    for(int i=0; i<m; i++){
+        cin >> b >> c;
+        cout << psum[c] - psum[b - 1] << "\n";
+    }
+    return 0;
+}
