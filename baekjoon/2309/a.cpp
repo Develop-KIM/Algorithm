5e24ccf81107995a300c4182bf68f29b3f11d723
Kimdonghwan <kimdonghwan913@gmail.com>
2024-09-28 19:49:04 +0900

    baekjoon 2309

--- baekjoon/2309/a.cpp
+++ baekjoon/2309/a.cpp
@@ -13,11 +13,13 @@ int main(){
 
     // 오름차순
     sort(a, a+9);
+
     do{
         int sum = 0;
         for(int i=0; i<7; i++) sum+=a[i];
         if(sum == 100) break;
     }while(next_permutation(a, a + 9));
+    
     for(int i=0; i<7; i++) cout << a[i] << "\n";
     return 0;
 }
