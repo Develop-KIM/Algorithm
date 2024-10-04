5eada2d39bbfcbfa22e24905a1e5ba1b0618c521
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-04 09:20:19 +0900

    2309

--- baekjoon/2309/v1/a.cpp
+++ baekjoon/2309/v1/a.cpp
@@ -1,20 +1,24 @@
+// 입력 9개를 받아 배열에 저장 arr[]
+// 배열을 오름차순으로 정렬 sort(arr, arr + 9)
+// 순열: 9개 중에 7개를 합하여 100 출력 do while(next_permutation(arr, arr + 9))
+
 #include <bits/stdc++.h>
 using namespace std;
 int arr[9];
 
 int main(){
-    for(int i=0; i<9; i++){
-        cin >> arr[i];
-    }
-    sort(arr, arr+9);
+    // 입력을 받아 배열에 저장
+    for(int i=0; i<9; i++) cin >> arr[i];
+
+    // 배열을 오름차순
+    sort(arr, arr + 9);
 
     do{
-        int sum=0;
-        for(int i=0; i<7; i++) sum += arr[i];
-        if(sum == 100) break;
-    }while(next_permutation(arr, arr+9));
-    
+        int sum = 0; // 각 순열마다 합산을 초기화를 위해 do 안에 넣어야 함
+        for(int i=0; i<7; i++) sum += arr[i]; // 배열에 있는 값들을 7개를 합산
+        if(sum == 100) break; // 합산한 값이 100이면 do ~ while 문 종료
+    }while(next_permutation(arr, arr + 9)); // 순열
     for(int i=0; i<7; i++) cout << arr[i] << '\n';
-
+    
     return 0;
 }
--- baekjoon/2309/v2/a.cpp
+++ baekjoon/2309/v2/a.cpp
@@ -1,27 +1,39 @@
+// 입력 9개를 받아 배열에 저장 arr[9] 및 배열의 전체합을 계산
+// 9개 중에 2개를 뽑아서 배열의 전체 합산을 뺀 값이 100을 찾는다.
+// 찾으면 새로운 배열에 2개를 빼고 저장 후 오름차순
+
 #include <bits/stdc++.h>
 using namespace std;
 int arr[9], sum;
 pair<int, int> ret;
 vector<int> v;
 
+// 전체 합 배열 안에 있는 2개를 뺐을 때 100을 찾아 ret에 저장하는 함수
 void solve(){
     for(int i=0; i<9; i++){
-        for(int j=0; j<i; j++){
-            if(sum - arr[i] - arr[j] == 100) 
-            ret = {i, j};
+        for(int j=0; j<i; i++){
+            if(sum - arr[i] - arr[j] == 100) ret = {i, j};
         }
     }
 }
 
 int main(){
+    // 9개를 입력받고 입력받은 수를 합산
     for(int i=0; i<9; i++){
         cin >> arr[i]; sum += arr[i];
     }
+    // 함수 호출
     solve();
+
+    // 저장된 ret의 첫 번째 수와 두 번째 수가 같을 경우 통과
+    // 저장된 수가 다를 경우 새로운 배열에 저장
     for(int i=0; i<9; i++){
         if(ret.first == i || ret.second == i) continue;
         v.push_back(arr[i]);
     }
+    // 오름차순
     sort(v.begin(), v.end());
     for(int i : v) cout << i << '\n';
+
+    return 0;
 }
