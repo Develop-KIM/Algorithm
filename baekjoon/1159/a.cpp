e7b26935d13126ad0c3c79a4d2d2356ce5530e08
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-10 07:59:35 +0900

    1159

--- baekjoon/1159/a.cpp
+++ baekjoon/1159/a.cpp
@@ -5,13 +5,13 @@ string str, ret;
 
 int main(){
     cin >> N;
-
+    
     for(int i=0; i<N; i++){
         cin >> str;
         cnt[str[0] - 'a']++;
     }
-    
+
     for(int i=0; i<26; i++) if(cnt[i] >= 5) ret += i + 'a';
-    if(ret.size()) cout << ret << '\n';
-    else cout << "PREDAJA" << '\n';
+    if(ret.size()) cout << ret;
+    else cout << "PREDAJA"; 
 }
