16eb98752f072fc85e6b1e2fa886fe492d65c92f
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-11 20:32:52 +0900

    9996

--- baekjoon/9996/a.cpp
+++ baekjoon/9996/a.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 int N, ast;
-string str, ori_str, pre, suf;
+string str, ori_str, prf, suf;
 
 int main(){
-    cin >> N;
-    cin >> ori_str;
+    cin >> N >> ori_str;
 
     ast = ori_str.find('*');
-    pre = ori_str.substr(0, ast);
+    prf = ori_str.substr(0, ast);
     suf = ori_str.substr(ast + 1);
-    
-    // cout << ast << '\n';
-    // cout << pre << '\n';
-    // cout << suf << '\n';
+
+    cout << ast << '\n';
+    cout << prf << '\n';
+    cout << suf << '\n';
 
     for(int i=0; i<N; i++){
         cin >> str;
-        if(pre.size() + suf.size() > str.size()){
-            cout << "NE\n";
+        if(prf.size() + suf.size() > str.size()){
+            cout << "NE" << '\n';
             continue;
-        } 
-        if(pre == str.substr(0, pre.size()) && suf == str.substr(str.size() - suf.size())) cout << "DA" << '\n';
+        }
+
+        if(prf == str.substr(0, prf.size()) && str.substr(str.size() - suf.size()) == suf) cout << "DA" << '\n';
         else cout << "NE" << '\n';
     }
-    return 0;
 }
