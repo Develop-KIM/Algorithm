baa7117841f5c03703e11cc0247d1fd31609226a
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-16 17:30:12 +0900

    1940

--- baekjoon/1940/a.cpp
+++ baekjoon/1940/a.cpp
@@ -1,15 +1,16 @@
+// 재료 수 N, 값옷을 만들려는 재료 수 M
+// N개만큼 배열에 저장 후 오름차순
+// 투 포인터 알고리즘 사용
+
 #include <bits/stdc++.h>
 using namespace std;
 int N, M;
 
 int main(){
     cin >> N >> M;
+    
     vector<int> materials(N);
-
-
-    for(int i=0; i<N; i++){
-        cin >> materials[i];
-    }
+    for(int i=0; i<N; i++) cin >> materials[i];
 
     sort(materials.begin(), materials.end());
     int left = 0; int right = N - 1;
@@ -28,5 +29,5 @@ int main(){
             right--;
         }
     }
-    cout << ret;
+    cout << ret << '\n';
 }
