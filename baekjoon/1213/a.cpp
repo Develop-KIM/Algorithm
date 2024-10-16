471847556c44548cbb7c09ef5cca3f590465fbd6
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-16 17:29:58 +0900

    1213

--- baekjoon/1213/a.cpp
+++ baekjoon/1213/a.cpp
@@ -7,22 +7,25 @@ char mid;
 int main(){
     cin >> str;
 
-    for(char a : str) cnt[(int) a]++;
+    for(char c : str){
+        cnt[(int) c]++;
+    }
+
     for(int i='Z'; i>='A'; i--){
         if(cnt[i]){
             if(cnt[i] & 1){
-                mid=char(i);
+                mid = char(i);
                 flag++;
                 cnt[i]--;
             }
-            if(flag == 2) break;
+            if(flag==2) break;
             for(int j=0; j<cnt[i]; j+=2){
-                ret = char(i) + ret;
-                ret += char(i);
+                ret = char(i) + ret; // 앞쪽 입력
+                ret += char(i); // 뒤쪽 입력
             }
         }
     }
     if(mid) ret.insert(ret.begin() + ret.size() / 2, mid);
-    if(flag==2) cout << "I'm Sorry Hansoo\n";
-    else cout << ret << "\n";
+    if(flag==2) cout << "I'm Sorry Hansoo";
+    else cout << ret;
 }
