1f4d2cdf65472d3daee8005b70240356eb264cee
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-18 15:20:12 +0900

    1629

--- baekjoon/1629/a.cpp
+++ baekjoon/1629/a.cpp
@@ -4,15 +4,14 @@ typedef long long ll;
 ll A, B, C;
 
 ll go(ll A, ll B){
-    if(B == 1) return A % C;
-    ll ret = go(A, B/2);
+    if(B == 1) return A & C;
+    ll ret = go(A, B / 2);
     ret = (ret * ret) % C;
     if(B % 2) ret = (ret * A) % C;
-    return ret;
+    return ret;  
 }
 
 int main(){
     cin >> A >> B >> C;
     cout << go(A, B) << "\n";
-
 }
