a329e8c76c769402c030ff099a048a9be3f87148
Kimdonghwan <kimdonghwan913@gmail.com>
2024-10-28 14:28:15 +0900

    항해99 1일차

---
+++ 향해/1일차/a.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool solution(string s)
+{
+    int p=0, y=0;
+    bool answer = true;
+    
+    for (char ch : s){
+        if(ch == 'p' || ch == 'P'){
+            p++;
+        } else if(ch == 'y' || ch == 'Y'){
+            y++;
+        }
+        if(p == y){
+            answer = true;
+        } else{
+            answer = false;
+        }
+    }
+    return answer;
+}
